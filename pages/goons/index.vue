90a68992c1b49e6e269a55f2b87827a7cc2117df
Grzegorz Szumilas <grzegorz.szumilas@ptag.pl>
2022-07-30 22:29:14 +0200

    add goons page

---
+++ pages/goons/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div >
+    {{ goons }}
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    goons: []
+  }),
+  async fetch() {
+    this.goons = await this.$http.$get('/api/get-goons')
+  },
+  fetchOnServer: false,
+})
+</script>
